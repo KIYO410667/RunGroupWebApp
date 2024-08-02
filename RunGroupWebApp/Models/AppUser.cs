33702cdc60cc8d24901070e83a0d8b54901817e5
KIYO <hyou0218@gmail.com>
2024-08-02 16:28:58 +0800

    Add EF migration and Data Seeding

--- RunGroupWebApp/Models/AppUser.cs
+++ RunGroupWebApp/Models/AppUser.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace RunGroupWebApp.Models
 {
     public class AppUser : IdentityUser
     {
+        [Key]
+        public string Id { get; set; }
         public Address? Address { get; set; }
         public int? Pace { get; set; }
         public int? Mileage { get; set; }
