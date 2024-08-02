e72634febef365cbf001ac16c1208cfdb120b832
KIYO <hyou0218@gmail.com>
2024-08-02 14:27:58 +0800

    Bulid Models for database

---
+++ RunGroupWebApp/Models/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RunGroupWebApp.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public Address? Address { get; set; }
+        public int? Pace { get; set; }
+        public int? Mileage { get; set; }
+    }
+}
