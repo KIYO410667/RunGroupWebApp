5190cd469b56fedfd5d1a0223e44615d9e640f19
KIYO <hyou0218@gmail.com>
2024-08-15 23:43:42 +0800

    Build Edit Page for Race, including Photo Deleting and Uploading

--- RunGroupWebApp/Controllers/RaceController.cs
+++ RunGroupWebApp/Controllers/RaceController.cs
@@ -69,5 +69,82 @@ namespace RunGroupWebApp.Controllers
                 return RedirectToAction("Index", new { message = "Photo uploaded successfully!" });
             }
         }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var userRace = await _raceRepository.GetById(id);
+            if(userRace == null) {return View("Error");}
+            EditRaceViewModel raceVM = new EditRaceViewModel
+            {
+                Address = userRace.Address,
+                Title = userRace.Title,
+                Description = userRace.Description,
+                URL = userRace.Image,
+                AddressId = userRace.AddressId,
+                RaceCategory = userRace.RaceCategory
+            };
+            return View(raceVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, EditRaceViewModel raceVM)
+        {
+            
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "輸入失敗，請重試");
+                return View("Error");
+            }
+            // 1. Find the userRace by Id
+            var userRace = await _raceRepository.GetById(id);
+            if(userRace == null) {
+                ModelState.AddModelError("", "使用者不存在，請重試");
+                return View("Error");
+            }
+
+            userRace.Id = raceVM.Id;
+            userRace.Title = raceVM.Title;
+            userRace.Description = raceVM.Description;
+            userRace.Address = raceVM.Address;
+            userRace.AddressId = raceVM.AddressId;
+            userRace.RaceCategory = raceVM.RaceCategory;
+
+            //2. Try to delete the existing photo, considering the case where it might not exist and user might not upload new photo
+            if (raceVM.Image != null)
+            {
+                if (!string.IsNullOrEmpty(userRace.Image))
+                {
+                    try
+                    {
+                        await _azureBlobService.DeletePhotoByUrlAsync(userRace.Image);
+                    }
+                    catch (Exception ex)
+                    {
+                        // Log the exception, but continue with the update process
+                        // The photo might not exist, or there might be other issues
+                        ModelState.AddModelError(string.Empty, "圖片刪除失敗");
+                        return View(raceVM);
+                    }
+                }
+                //3. Upload the photo to AzureBlob if there's a file being uploading
+                using (var stream = raceVM.Image.OpenReadStream())
+                {
+                    var fileName = Guid.NewGuid().ToString() + Path.GetExtension(raceVM.Image.FileName);
+                    var blobUrl = await _azureBlobService.UploadPhotoAsync(stream, fileName);
+                    if (blobUrl == null)
+                    {
+                        ModelState.AddModelError("Image", "Photo upload failed");
+                        return View(raceVM);
+                    }
+                    userRace.Image = blobUrl; //only change the Image URL
+                }
+
+            }
+            
+            _raceRepository.Update(userRace);
+
+            return RedirectToAction("Index");
+
+        }
     }
 }
