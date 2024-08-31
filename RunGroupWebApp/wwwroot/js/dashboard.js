e67a81e805d98113658f24ff0837d49ea0191a8c
KIYO <hyou0218@gmail.com>
2024-09-01 00:41:09 +0800

    Dashboard and Edit-profile frontend

---
+++ RunGroupWebApp/wwwroot/js/dashboard.js
@@ -0,0 +1,19 @@
+﻿
+    function showSection(section) {
+        document.getElementById('racesSection').style.display = 'none';
+    document.getElementById('clubsSection').style.display = 'none';
+    document.getElementById('racesTab').classList.remove('active');
+    document.getElementById('clubsTab').classList.remove('active');
+
+    if (section === 'races') {
+        document.getElementById('racesSection').style.display = 'block';
+    document.getElementById('racesTab').classList.add('active');
+    }
+    else if (section === 'clubs') {
+        document.getElementById('clubsSection').style.display = 'block';
+    document.getElementById('clubsTab').classList.add('active');
+    }
+}
+
+    // Default to showing races
+    showSection('races');
---
+++ RunGroupWebApp/wwwroot/js/profile-edit.js
@@ -0,0 +1,26 @@
+﻿// Form validation
+(function () {
+    'use strict'
+    var forms = document.querySelectorAll('.needs-validation')
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
+
+// Profile image preview
+document.getElementById('ProfilePhotoFile').addEventListener('change', function (e) {
+    if (e.target.files && e.target.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            document.getElementById('profileImage').setAttribute('src', e.target.result);
+        }
+        reader.readAsDataURL(e.target.files[0]);
+    }
+});
